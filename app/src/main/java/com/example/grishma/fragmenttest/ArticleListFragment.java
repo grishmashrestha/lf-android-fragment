6a9d04a57f03f94404554d18ade2054d3277a140
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-02-18 17:46:15 +0545

    initial commit

---
+++ app/src/main/java/com/example/grishma/fragmenttest/ArticleListFragment.java
@@ -0,0 +1,47 @@
+package com.example.grishma.fragmenttest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ArticleListFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ArticleListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ArticleListFragment extends Fragment {
+
+
+    public ArticleListFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     * @return A new instance of fragment ArticleListFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ArticleListFragment newInstance(String param1, String param2) {
+        ArticleListFragment fragment = new ArticleListFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_article_list, container, false);
+    }
+}
