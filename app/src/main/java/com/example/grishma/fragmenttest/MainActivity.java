03ee1d29bb87117790421140237665477c77221d
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-02-19 10:36:58 +0545

    displayed content of fragment without the base activity due to problem in toolbar title

--- app/src/main/java/com/example/grishma/fragmenttest/MainActivity.java
+++ app/src/main/java/com/example/grishma/fragmenttest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.grishma.fragmenttest;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -8,15 +10,16 @@ import com.example.grishma.fragmenttest.base.BaseActivity;
 
 import timber.log.Timber;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Timber.i("from onCreate at mainactivity");
         setContentView(R.layout.activity_main);
-
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Fragment Test");
     }
 
     @Override
